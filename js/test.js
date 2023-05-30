9f202023c40f1a7f185d6312bab45660fd8de285
Ashikensan <kenji_ashi@yahoo.co.jp>
2023-05-31 01:08:19 +0900

    addボタン押下のjs追加

--- js/test.js
+++ js/test.js
@@ -1,1 +1,8 @@
-alert(11);
+//addボタンを押下した時の挙動
+$("#add").on("click",function(e){
+    e.preventDefault();//ボタンクリックによるページのリロードを防ぐ
+    const taskName = $("#taskName").val();
+    const html =`<tr><th>`+ taskName + `</th></tr>`;  
+    $("#taskList").append(html);
+    $("#taskName").val("");//入力フィールドをクリアする
+})
